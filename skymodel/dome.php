87bb569804d0b6df3128e1cd68ff5eaa148fb67f
john <bblodfon@gmail.com>
2018-07-08 22:30:43 +0100

    Changed execution of dome_points to linux-style format

--- skymodel/dome.php
+++ skymodel/dome.php
@@ -4,7 +4,7 @@ $turbidity=$_GET['turbidity'];
 $solar_elevation=$_GET['solar_elevation'];
 $albedo=$_GET['albedo'];
 
-$callExp1="dome_points.exe " . $turbidity . " " . $solar_elevation;
+$callExp1="./dome_points " . $turbidity . " " . $solar_elevation;
 $callExp2=$albedo;
 $callExp=$callExp1 . " " . $callExp2;
 
